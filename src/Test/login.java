7925e4e4b1718fde15f1b36616326c5614518842
Mahesh <Mahesh@DESKTOP-ES6JCAK>
2021-07-16 18:22:55 +0530

    My first commint using git comming....

---
+++ src/Test/login.java
@@ -0,0 +1,18 @@
+package Test;
+
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name = "rushi";
+		
+		if(name == "rushi") {
+			System.out.println("Hi this is rushikesh");
+		}
+		else {
+			System.out.println("Sorry, this is not rushikesh....");
+		}
+	}
+
+}
