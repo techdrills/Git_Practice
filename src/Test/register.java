e24ce0fd4e417443281963cfaff95651fc68549e
Mahesh <Mahesh@DESKTOP-ES6JCAK>
2021-07-18 12:07:33 +0530

    in local branch

--- src/Test/register.java
+++ src/Test/register.java
@@ -5,6 +5,9 @@ public class register {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("register here....");
+
+
+System.out.println("softfeat work....");
 	}
 
 }
