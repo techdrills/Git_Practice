b1be0b782c20de8ac72e11180b73658866efa98f
Mahesh <Mahesh@DESKTOP-ES6JCAK>
2021-07-18 12:14:55 +0530

    local git

--- src/Test/softfeat.java
+++ src/Test/softfeat.java
@@ -4,7 +4,7 @@ public class softfeat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("softfeat.....");
+System.out.println("ferewrewrewrew.......");
 	}
 
 }
